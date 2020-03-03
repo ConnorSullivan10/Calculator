72e63d32573cde5aa33a2e3fcb72bec00e222b26
Connor Sullivan <cksullvn@gmail.com>
2020-03-03 13:55:25 -0600

    finished 2nd half of calculator assignment

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -20,8 +20,8 @@ namespace Calculator
 
 
             Console.WriteLine("Now before you enter your next set of numbers separated by commas, would you like to square or multiply them?");
-            string operand = Console.ReadLine().ToLower();
-            if (operand == "multiply")
+            string operator1 = Console.ReadLine().ToLower();
+            if (operator1 == "multiply")
             {
                 Console.WriteLine("Enter the comma separated numbers that you'd like multiplied");
                 string numsToMultiply = Console.ReadLine();
@@ -35,7 +35,7 @@ namespace Calculator
                 Console.WriteLine(multipliedSequenceStart);
                 Console.ReadLine();
             }
-            else if (operand == "square")
+            else if (operator1 == "square")
             {
                 Console.WriteLine("Enter the comma separated numbers that you'd like squared");
                 string numsToSquare = Console.ReadLine();
@@ -51,6 +51,78 @@ namespace Calculator
                 char[] charToTrim = { ',' };
                 Console.WriteLine(squaredSequence.TrimEnd(charToTrim));
                 Console.ReadLine();
+            }
+
+            Console.WriteLine("Enter a series of comma separated numbers as well as a prefix to indicate whether you'd like the numbers to be multiplied, squared, added, divided, or averaged (*, ^, +, /, avg) ");
+            string initialEntry = Console.ReadLine().ToLower();
+            char operator2 = initialEntry[0];
+            if (initialEntry.Substring(0, 3) == "avg")
+            {
+                char[] charToTrim2 = { 'a', 'v', 'g' };
+                string trimmedAvg = initialEntry.TrimStart(charToTrim2);
+                string[] splitTrimmedAvg = trimmedAvg.Split(',');
+                int sum = 0;
+                for (var i = 0; i < splitTrimmedAvg.Length; i++)
+                {
+                    sum += int.Parse(splitTrimmedAvg[i]);
+                }
+                var average = sum / splitTrimmedAvg.Length;
+                Console.WriteLine(average);
+            }
+            else
+            {
+                char[] charToTrim3 = { '*', '^', '+', '/' };
+                string trimmedEntry = initialEntry.TrimStart(charToTrim3);
+                string[] splitInitialEntry = trimmedEntry.Split(',');
+                switch (operator2)
+                {
+                    case '*':
+                        int multiplySequenceStart = 1;
+                        foreach (var entry in splitInitialEntry)
+                        {
+                            int singleNum = int.Parse(entry);
+                            multiplySequenceStart *= singleNum;
+                        }
+                        Console.WriteLine(multiplySequenceStart);
+                        break;
+                    case '^':
+                        string squaredSequence = "";
+                        foreach (var entry in splitInitialEntry)
+                        {
+                            int singleNum = int.Parse(entry);
+                            var squared = singleNum * singleNum;
+                            string squaredString = squared.ToString() + ",";
+                            squaredSequence += squaredString;
+                        }
+                        char[] endComma = { ',' };
+                        Console.WriteLine(squaredSequence.TrimEnd(endComma));
+                        break;
+                    case '+':
+                        int additionSequenceStart = 0;
+                        foreach (var entry in splitInitialEntry)
+                        {
+                            int singleNum = int.Parse(entry);
+                            additionSequenceStart += singleNum;
+                        }
+                        Console.WriteLine(additionSequenceStart);
+                        break;
+                    case '/':
+                        var divisionStart = 0m;
+                        for (var k = 0; k < splitInitialEntry.Length; k++)
+                        {
+                            if (k == 0)
+                            {
+                                divisionStart = Convert.ToDecimal(splitInitialEntry[k]);
+                            }
+                            else
+                            {
+                                divisionStart /= Convert.ToDecimal(splitInitialEntry[k]);
+                            }
+                        }
+                        Console.WriteLine(divisionStart);
+                        break;
+                }
+
             }
         }
     }
